066d4d6d98c615e6d78dfe7c4b689503591f4e79
KhoaNguyen0205 <ngockhoa2k2@gmail.com>
2023-03-20 13:12:55 +0700

    buoi 2 sprint 2

--- Booking-app/api/index.js
+++ Booking-app/api/index.js
@@ -2,18 +2,21 @@ const express = require('express');
 const cors = require('cors');
 const { default: mongoose } = require('mongoose');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
 const User = require('./models/User.js'); 
+const cookieParser = require('cookie-parser'); 
 require('dotenv').config();
 const app = express();
 
 const bcryptSalt = bcrypt.genSaltSync(10);
+const jwtSecret = 'ngockhoangockhoangockhoa'; 
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(cors({
     credentials: true,
-    origin: 'http://localhost:5173',
+    origin: 'http://localhost:5173'
 }))
-
 
 mongoose.connect(process.env.MONGO_URL)
 
@@ -36,14 +39,44 @@ app.post('/register' , async (req , res)=>{
 
 });
 
-app.post('/login', async (req,res) => {
-    const {email,password} = req.body;
-    const userDoc = await User.findOne({email});
-    if (userDoc) {
-       res.json('found');
-    }else {
-        res.json('not found');
+app.post('/login' , async (req , res) => { 
+  const {email,password} = req.body;
+  const userDoc = await User.findOne({email});
+  if(userDoc) {
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if(passOk) {
+        jwt.sign({
+            email:userDoc.email, 
+            id:userDoc._id, 
+            name: userDoc.name
+        }, jwtSecret,{},(err,token) => {
+            if(err) throw err;
+            res.cookie('token', token).json(userDoc);
+        });
+    }else{
+        res.status(422).json('pass not ok');
     }
-})
+  }else{
+    res.json('not found');
+  }
+});
+
+
+app.get('/profile', (req,res) => {
+    const {token} = req.cookies;
+    if(token) { 
+        jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+            if(err) throw err;
+           const {name,email,_id} = await User.findById(userData.id);
+            res.json({name,email,_id});
+        });
+    }else {
+        res.json(null);
+    }
+});
+
+app.post('/logout', (req, res) => {
+   res.cookie('token', '').json(true);
+});
 
 app.listen(4000);
--- Booking-app/client/src/pages/IndexPage.jsx
+++ Booking-app/client/src/pages/IndexPage.jsx
@@ -1,12 +1,86 @@
 import { Link } from "react-router-dom";
+import FooterPage from "../Footer";
 import Header from "../Header";
 
 export default function IndexPage() {
     return (
-      <div>
-        ALL Information in here
+        <div>
+        <section className="hotel-menu" >
+        <div className="container">
+
+         <div className="menu-box">
+          <div className="hotel-img">
+            <img src="https://i.pinimg.com/236x/14/c0/5b/14c05ba8f611c1d3cc957792bbcfbfee.jpg"  className="img-responsive img-curve" alt="" />
+          </div>
+          <div className="title-hotel">
+              <h4>DALAT WIND HOTEL</h4>
+              <p>Uniquely designed organuc nature house, off-grid</p>
+              <h4>$120 per night</h4>
+          </div>
+         </div>
+
+         <div className="menu-box">
+          <div className="hotel-img">
+            <img src="https://i.pinimg.com/236x/1f/8f/aa/1f8faa1528a439fab13b55fcdf912b97.jpg" className="img-responsive img-curve" alt="" />
+          </div>
+          <div className="title-hotel">
+              <h4>RAON VILLA</h4>
+              <p>Breathtaking Mountain view in cozy</p>
+              <h4>$300 per night</h4>
+          </div>
+         </div>
+
+         <div className="menu-box">
+          <div className="hotel-img">
+            <img src="https://i.pinimg.com/564x/e3/93/9a/e3939a042d1e3afc289b865e829e6038.jpg" className="img-responsive img-curve" alt="" />
+          </div>
+          <div className="title-hotel">
+              <h4>HERA BOUTIQUE HOTEL</h4>
+              <p>Rooms at Hera Boutique Hotel are fitted with a seating area</p>
+              <h4>$180 per night</h4>
+          </div>
+         </div>
+
+         <div className="menu-box">
+          <div className="hotel-img">
+            <img src="https://i.pinimg.com/236x/03/af/9c/03af9c07d1ad30c465bb2596849aece0.jpg" className="img-responsive img-curve" alt="" />
+          </div>
+          <div className="title-hotel">
+              <h4>JOLIE HOUSE</h4>
+              <p>Each unit has a kitchen area with a microwave and a fridge</p>
+              <h4>$190 per night</h4>
+          </div>
+         </div>
+
+         <div className="menu-box">
+          <div className="hotel-img">
+            <img src="https://i.pinimg.com/236x/7d/6e/b3/7d6eb3eae48d464c1477272c61f5aef3.jpg"  className="img-responsive img-curve" alt="" />
+          </div>
+          <div className="title-hotel">
+              <h4>DALAT WIND HOTEL</h4>
+              <p>Uniquely designed organuc nature house, off-grid</p>
+              <h4>$120 per night</h4>
+          </div>
+         </div>
+
+
+         <div className="menu-box">
+          <div className="hotel-img">
+            <img src="https://i.pinimg.com/236x/7f/16/ce/7f16ce8a1bbd5cd4ad86595876512376.jpg"  className="img-responsive img-curve" alt="" />
+          </div>
+          <div className="title-hotel">
+              <h4>DALAT WIND HOTEL</h4>
+              <p>Uniquely designed organuc nature house, off-grid</p>
+              <h4>$120 per night</h4>
+          </div>
+         </div>     
+         <div className="clearfix"> </div>
       </div>
-      
-   
+      </section>
+       
+       <FooterPage />
+     
+      </div>
     )
+    
 } 
